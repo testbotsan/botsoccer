8c720e6d626f7fb1637de5a895c64deb5b35eb15
sirasawa <20172109@kcska.onmicrosoft.com>
2020-03-20 00:42:33 +0900

    # Please enter the commit message for your changes. Lines starting

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -28,6 +28,8 @@ import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.replyText;
+import com.linecorp.bot.model.replyToken;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
