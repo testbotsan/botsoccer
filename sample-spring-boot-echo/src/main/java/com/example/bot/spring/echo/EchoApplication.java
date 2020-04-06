8271f1a3b730f7af9f21a66c3071a79fe3e3f8b4
sirasawa <20172109@kcska.onmicrosoft.com>
2020-04-06 10:18:27 +0900

    # Please enter the commit message for your changes. Lines starting

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -61,7 +61,6 @@ import java.sql.Statement;
 import lombok.NonNull;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
-import sun.security.util.Password;
 
 @SpringBootApplication
 @LineMessageHandler
